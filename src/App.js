7f8c5be37571ccd2fb08510e600c33a2e6d61343
Ramya Gunashekara <ramya.gunashekar@gmail.com>
2023-04-19 13:36:40 +1000

    Adding authenticator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+
 import DropDown from "./components/DropDown";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-
-function App() {
-
+function App({ signOut }) {
   return (
-    <div>
-      <DropDown></DropDown>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <DropDown></DropDown>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
